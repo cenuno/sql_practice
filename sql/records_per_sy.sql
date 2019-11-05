46fa4821a0b42b0a879b9c8bb0e90f843fa68aca
cenuno <24255819+cenuno@users.noreply.github.com>
2019-11-05 15:16:27 -0800

    Remove sql files

--- sql/records_per_sy.sql
+++
@@ -1,5 +0,0 @@
--- how many records per school year
-SELECT school_year, COUNT(school_year)
-FROM cps_dropout_rate_2011_2019
-GROUP BY school_year
-ORDER BY school_year;
--- sql/top_ten_cca_by_crime.sql
+++
@@ -1,6 +0,0 @@
--- Top 10 community areas in terms of number of crimes, 2019
-SELECT COUNT(id) AS count, "Community Area" AS cca
-FROM crimes_2019
-GROUP BY cca
-ORDER BY count DESC
-LIMIT 10;
